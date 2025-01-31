5e29ff40a39cb8ba5c410bdc6d163867f58fb063
MarekLukk <mareklukk8@gmail.com>
2025-01-31 13:56:32 +0200

    codeerimine

--- VigadeOtsing.py
+++ VigadeOtsing.py
@@ -1,28 +1,28 @@
-#Str8(Oli parandatud "),str16(Oli parandatud"),str12(oli eemaldatud")")
-import * from math
+#Str8(Oli parandatud "),str16(Oli parandatud"),str12(oli eemaldatud")"),Str2(oli parandatud syntaks)
+from math import *
 print("Ruudu karakteristikud")
-a=input('Sisesta ruudu k¸lje pikkus => ')
+a=input('Sisesta ruudu k√ºlje pikkus => ')
 S=a**2
 print("Ruudu pindala", S)
 P=4*a
-print("Ruudu ¸mbermııt", P)
+print("Ruudu √ºmberm√µ√µt", P)
 di=a*math.sqr(2)
 print("Ruudu diagonaal", round(di,2))
 print()
-print("Ristk¸liku karakteristikud"))
-b=input("Sisesta ristk¸liku 1. k¸lje pikkus => ")
-c=input("Sisesta ristk¸liku 2. k¸lje pikkus => ")
+print("Ristk√ºliku karakteristikud"))
+b=input("Sisesta ristk√ºliku 1. k√ºlje pikkus => ")
+c=input("Sisesta ristk√ºliku 2. k√ºlje pikkus => ")
 S=b*c
-print("Ristk¸liku pindala", S)
+print("Ristk√ºliku pindala", S)
 P=2(b+c)
-print("Ristk¸liku ¸mbermııt", P)
+print("Ristk√ºliku √ºmberm√µ√µt", P)
 di=math.sqrt(b*2+c*2)
-print("Ristk¸liku diagonaal", round(di)
+print("Ristk√ºliku diagonaal", round(di)
 print()
 print("Ringi karakteristikud")
 r=input(''Sisesta ringi raadiusi pikkus => ''))
 d=2r
-print("Ringi l‰bimııt" d)
+print("Ringi l√§bim√µ√µt" d)
 S=pi()*r*2
 print("Ringi pindala", round(S))
 C=2pi()*r
